95f6b1d0862a044d2a089a3664a839e4f92dd5d0
maysapereira <maysalvespereira@gmail.com>
2022-07-16 21:53:57 -0300

    :card_file_box: conecta banco de dados com GORM

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/maysapereira/go-api-rest-gin/database"
 	"github.com/maysapereira/go-api-rest-gin/models"
 	"github.com/maysapereira/go-api-rest-gin/routes"
 )
 
 func main() {
+	database.ConectaBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Maysa Pereira", CPF: "123.456.789-01", RG: "98.765.43"},
 		{Nome: "Kasper", CPF: "987.654.321-09", RG: "12.345.67"},
