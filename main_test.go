15d6f645a453ac478d3c6c6736cee0f04176f5d7
maysapereira <maysalvespereira@gmail.com>
2022-08-02 11:33:35 -0300

    :sparkles: adiciona teste no método POST que exibe todos os alunos

--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/maysapereira/go-api-rest-gin/controllers"
+	"github.com/maysapereira/go-api-rest-gin/database"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,4 +29,14 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
 
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
